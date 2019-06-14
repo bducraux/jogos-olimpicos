0e81a7fb893cf094c60e76c02fb8f4eddd442e49
Bruno <bruno.drx@gmail.com>
2019-06-14 03:03:06 -0300

    adicionado testes para os endpoints da Competicao

--- cob/tests/test_competicao_endpoints.py
+++ cob/tests/test_competicao_endpoints.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from ..models import Competicao
 from ..serializers import CompeticaoSerializer
 from .base_api_test import BaseApiTest
+import json
 
 
 class CompeticaoTest(BaseApiTest):
@@ -14,45 +15,129 @@ class CompeticaoTest(BaseApiTest):
         super().setUp()
 
         # cria competicoes
-        self.dardos_eliminatoria = Competicao.objects.create(
-            nome="Dardos Eliminatoria Teste",condicao_para_vitoria="maior_de_tres")
-        self.cem_metros_eliminatoria = Competicao.objects.create(
-            nome="100 metros Eliminatoria Teste", condicao_para_vitoria="menor")
-        self.dardos_final = Competicao.objects.create(
-            nome="Dardos Final Teste", condicao_para_vitoria="maior_de_tres")
-        self.cem_metros_final = Competicao.objects.create(
-            nome="100 metros Final Teste", condicao_para_vitoria="menor")
+        self.competicao = Competicao.objects.create(
+            nome="Dardos Eliminatoria Teste", condicao_para_vitoria="maior_de_tres"
+        )
+        self.competicao2 = Competicao.objects.create(
+            nome="Cem Metros Teste", condicao_para_vitoria="menor"
+        )
 
-    # TESTES DO MÓDULO
+        # payload válido para create e update
+        self.payload_valido = {
+            'nome': 'Teste 1',
+            'condicao_para_vitoria': 'menor',
+            'status': 'aguardando'
+        }
 
+        # payload inválido para create e update
+        self.payload_invalido = {
+            'nome': '',
+            'condicao_para_vitoria': 'menor',
+            'status': 'aguardando'
+        }
 
-
-    # TESTES DA API
-
-    def test_competicao_endpoint_list(self):
-        """
-        Esse teste garante que todas as competicoes adicionadas mo método setUp
-        existem quando fizermos um GET request para  competicao/ endpoint
-        """
-
+    def test_competicao_recuperar_todas(self):
         response = self.client.get(reverse('competicao-list'))
 
-        expected = Competicao.objects.all()
+        competicoes = Competicao.objects.all()
+        serializer = CompeticaoSerializer(competicoes, many=True)
 
-        serialized = CompeticaoSerializer(expected, many=True)
-        self.assertEqual(response.data, serialized.data)
+        self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_competicao_endpoint_por_id(self):
-        """
-        Esse teste garante que a competicao adicionada mo método setUp
-        existe quando fizermos um GET request para  competicao/1 endpoint
-        """
-
-        response = self.client.get(reverse('competicao-detail', kwargs={'pk': 1}))
-
-        expected = Competicao.recuperar_registro_pelo_id(1)
-
-        serialized = CompeticaoSerializer(expected)
-        self.assertEqual(response.data, serialized.data)
+    def test_competicao_recuperar_uma_valida(self):
+        response = self.client.get(
+            reverse('competicao-detail', kwargs={'pk': self.competicao.id}))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def est_competicao_recuperar_uma_invalida(self):
+        response = self.client.get(
+            reverse('competicao-detail', kwargs={'pk': 30}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_competicao_criacao_valida(self):
+        response = self.client.post(
+            reverse('competicao-list'),
+            data=json.dumps(self.payload_valido),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_competicao_criacao_invalida(self):
+        response = self.client.post(
+            reverse('competicao-list'),
+            data=json.dumps(self.payload_invalido),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_competicao_update_valido(self):
+        response = self.client.put(
+            reverse('competicao-detail', kwargs={'pk': self.competicao.id}),
+            data=json.dumps(self.payload_valido),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_competicao_update_invalido(self):
+        response = self.client.put(
+            reverse('competicao-detail', kwargs={'pk': self.competicao.id}),
+            data=json.dumps(self.payload_invalido),
+            content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_competicao_patch_valido(self):
+        payload = {
+            'status': 'encerrada'
+        }
+
+        response = self.client.patch(
+            reverse('competicao-detail', kwargs={'pk': self.competicao.id}),
+            data=json.dumps(payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_competicao_patch_invalido(self):
+        payload = {
+            'status': 'encerrada'
+        }
+
+        response = self.client.patch(
+            reverse('competicao-detail', kwargs={'pk': 30}),
+            data=json.dumps(payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_competicao_patch_status_invalido(self):
+        payload = {
+            'status': 'encerradaaaaa'
+        }
+        response = self.client.patch(
+            reverse('competicao-detail', kwargs={'pk': self.competicao.id}),
+            data=json.dumps(payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_competicao_patch_condicao_para_vitoria_invalido(self):
+        payload = {
+            'condicao_para_vitoria': 'aaa'
+        }
+        response = self.client.patch(
+            reverse('competicao-detail', kwargs={'pk': self.competicao.id}),
+            data=json.dumps(payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_competicao_delete_valido(self):
+        response = self.client.delete(
+            reverse('competicao-detail', kwargs={'pk': self.competicao.pk}))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_competicao_delete_invalido(self):
+        response = self.client.delete(
+            reverse('competicao-detail', kwargs={'pk': 30}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)

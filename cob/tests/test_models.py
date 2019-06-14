a5f03fc42f4f0060edf3b8e00ebb2c075fb3c77e
Bruno <bruno.drx@gmail.com>
2019-06-14 01:13:08 -0300

    adiciona testes para o models.py

--- cob/tests/test_models.py
+++ cob/tests/test_models.py
@@ -2,23 +2,47 @@ from django.test import TestCase
 from ..models import Competicao, Atleta, Resultado
 
 
-class ModelsTest(TestCase):
+class CompeticaoTest(TestCase):
 
-    def SetUp(self):
-        pass
+    def setUp(self):
+        # cria a competição
+        c = Competicao.objects.create(
+            nome='Dardos Eliminatoria Teste', condicao_para_vitoria='maior_de_tres'
+        )
 
-    # Testa pegar uma competição passando o id
-    def test_recupera_registro_pelo_id(self):
-        # cria uma competição
-        Competicao.objects.create(
-            nome="Dardos Eliminatoria Teste", condicao_para_vitoria="maior_de_tres")
+        # cria Atleta
+        a = Atleta.objects.create(nome='Teste')
 
-        expected = "Dardos Eliminatoria Teste"
+        # cria resultado
+        Resultado.objects.create(
+            competicao=c,
+            atleta=a,
+            resultado='20.12'
+        )
 
-        competicao = Competicao().recuperar_registro_pelo_id(1)
+    def test_competicao_criacao(self):
+        # recupera a competicao criada
+        competicao = Competicao.objects.get(id=1)
 
-        self.assertEqual(expected, competicao.nome)
+        expected_object_name = f'{competicao.__str__()}'
 
-    def test_recuperar_por_id(selfs):
-        # competicao_obj = Competicao.objects.get(pk=1)
-        pass
+        self.assertEquals(expected_object_name, 'Dardos Eliminatoria Teste')
+        self.assertTrue(isinstance(competicao, Competicao))
+
+    def test_atleta_criacao(self):
+        # recupera o atleta criado
+        atleta = Atleta.objects.get(id=1)
+
+        expected_object_name = f'{atleta.__str__()}'
+
+        self.assertEquals(expected_object_name, 'Teste')
+        self.assertTrue(isinstance(atleta, Atleta))
+
+    def test_resultado_criacao(self):
+        # recupera o resultado criado
+        resultado = Resultado.objects.get(id=1)
+
+        expected_object_name = f'{resultado.__str__()}'
+
+        self.assertEquals(expected_object_name, 'Dardos Eliminatoria Teste - Teste - 20.120')
+        self.assertTrue(isinstance(resultado, Resultado))
